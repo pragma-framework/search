bd1c197913676930bbb237deec8a6ece98189704
Cyril Rouyer <cyril.rouyer@gmail.com>
2017-06-12 13:10:22 +0200

    Fix / Search preindex > Fix 'was_new' detection

--- Pragma/Search/Searchable.php
+++ Pragma/Search/Searchable.php
@@ -4,9 +4,11 @@ namespace Pragma\Search;
 trait Searchable{
 	protected $indexed_cols = [];
 	protected $infile_cols = [];
+	protected $index_was_new = true;
 
 	protected function index_cols(){
 		$this->indexed_cols = array_flip(func_get_args());
+		$this->pushHook('before_save', 'init_index_was_new');
 		$this->pushHook('after_save', 'index_prepare');
 		$this->pushHook('after_open', 'index_init_values');
 
@@ -27,14 +29,18 @@ trait Searchable{
 		}
 	}
 
+	protected function init_index_was_new(){
+		$this->index_was_new = $this->is_new();
+	}
+
 	protected function index_prepare($last = false){
 		if( empty($this->indexed_cols) ){
 			$this->index_init_values($last);
 			return false;
 		}
 
-		if( $this->is_new() ){
-			foreach($this->indexed_cols as $col){
+		if( $this->index_was_new ){
+			foreach($this->indexed_cols as $col => $value){
 				PendingIndexCol::store($this, $col, isset($this->infile_cols[$col]));
 			}
 		}
