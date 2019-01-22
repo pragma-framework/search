b2125da4bf2535c12ac03229be2525e5c4b9dcf2
thomas-netlor <thomas@netlor.fr>
2019-01-22 09:49:35 +0100

    Set Up & Down fct for AllowNullOnIndexesContextId
    changeColumn can't be revert

--- db/migrations/20181025151437_allow_null_on_indexes_context_id.php
+++ db/migrations/20181025151437_allow_null_on_indexes_context_id.php
@@ -3,7 +3,7 @@
 use Phinx\Migration\AbstractMigration;
 
 class AllowNullOnIndexesContextId extends AbstractMigration {
-	public function change() {
+	public function up() {
 		if(defined('ORM_ID_AS_UID') && ORM_ID_AS_UID){
 			$strategy = ! defined('ORM_UID_STRATEGY') ? 'php' : ORM_UID_STRATEGY;
 			$t = $this->table('indexes');
@@ -21,6 +21,26 @@ class AllowNullOnIndexesContextId extends AbstractMigration {
 		else{
 			$t = $this->table('indexes');
 			$t->changeColumn('context_id', 'integer', ["null" => true])->update();
+		}
+	}
+	public function down(){
+		if(defined('ORM_ID_AS_UID') && ORM_ID_AS_UID){
+			$strategy = ! defined('ORM_UID_STRATEGY') ? 'php' : ORM_UID_STRATEGY;
+			$t = $this->table('indexes');
+			switch($strategy){
+				case 'mysql':
+				case 'laravel-uuid':
+					$t->changeColumn('context_id', 'char')->update();
+					break;
+				default:
+				case 'php':
+					$t->changeColumn('context_id', 'char')->update();
+					break;
+			}
+		}
+		else{
+			$t = $this->table('indexes')
+				->changeColumn('context_id', 'integer')->update();
 		}
 	}
 }
